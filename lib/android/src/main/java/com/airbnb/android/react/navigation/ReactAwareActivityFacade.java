9a072401447e8e2d4f2882ef00043e45f0b618af
Felipe Lima <felipe.lima@gmail.com>
2016-12-20 11:36:53 -0800

    Initial Android work

--- lib/android/src/main/java/com/airbnb/android/react/navigation/ReactAwareActivityFacade.java
+++ lib/android/src/main/java/com/airbnb/android/react/navigation/ReactAwareActivityFacade.java
@@ -5,11 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 interface ReactAwareActivityFacade {
-    void runOnUiThread(Runnable action);
-    Context getBaseContext();
-    ReactToolbarFacade getSupportActionBar();
-    void startActivityForResult(Intent intent, int requestCode, Bundle bundle);
-    void startActivityForResult(Intent intent, int requestCode);
-    void setResult(int resultCode, Intent resultIntent);
-    void finish();
+  // @formatter:off
+  void runOnUiThread(Runnable action);
+  Context getBaseContext();
+  void startActivityForResult(Intent intent, int requestCode, Bundle bundle);
+  void startActivityForResult(Intent intent, int requestCode);
+  void setResult(int resultCode, Intent resultIntent);
+  void finish();
+  // @formatter:on
 }
