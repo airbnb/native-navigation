75e4a3a14a00fc1b6bc4b038edba9dde7ba37744
Felipe Lima <felipe.lima@gmail.com>
2016-12-20 11:37:09 -0800

    Add missing files

---
+++ lib/android/src/main/java/com/airbnb/android/react/navigation/BundleBuilder.java
@@ -0,0 +1,13 @@
+package com.airbnb.android.react.navigation;
+
+import android.os.Bundle;
+
+/** A Bundle that doesn't suck. Allows you to chain method calls as you'd expect. */
+public class BundleBuilder extends ExtendableBundleBuilder<BundleBuilder> {
+  public BundleBuilder() {
+  }
+
+  public BundleBuilder(Bundle bundle) {
+    putAll(bundle);
+  }
+}
---
+++ lib/android/src/main/java/com/airbnb/android/react/navigation/ReactToolbar.java
@@ -0,0 +1,10 @@
+package com.airbnb.android.react.navigation;
+
+import android.content.Context;
+import android.support.v7.widget.Toolbar;
+
+public abstract class ReactToolbar extends Toolbar {
+  public ReactToolbar(Context context) {
+    super(context);
+  }
+}
