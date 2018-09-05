7c4135f3bd86b50a7853d76b29601d43c919f042
shaddeen <im@shaddeen.com>
2018-09-05 17:08:11 +0100

    Clear menu on rightTitle

--- lib/android/src/main/java/com/airbnb/android/react/navigation/DefaultNavigationImplementation.java
+++ lib/android/src/main/java/com/airbnb/android/react/navigation/DefaultNavigationImplementation.java
@@ -618,6 +618,7 @@ public class DefaultNavigationImplementation implements NavigationImplementation
             }
         }
         if (stringHasChanged("rightTitle", prev, next)) {
+            menu.clear();
             if (next.hasKey("rightTitle")) {
                 String rightTitle = next.getString("rightTitle");
                 MenuItem item = menu.add(rightTitle);
