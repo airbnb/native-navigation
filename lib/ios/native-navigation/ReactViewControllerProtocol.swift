f7d178fc27480b1918e0a153b20cc9bc17f147fc
Almouro <contact@almouro.com>
2018-10-24 11:59:52 +0200

    Fix isCurrentlyTransitioning being true on iOS when resetting screens

--- lib/ios/native-navigation/ReactViewControllerProtocol.swift
+++ lib/ios/native-navigation/ReactViewControllerProtocol.swift
@@ -247,67 +247,75 @@ extension UINavigationController {
       }
     }
   }
+
+  func completeViewControllerTransition(_ viewController: InternalReactViewControllerProtocol) {
+    viewController.isCurrentlyTransitioning = false
+    // The completion handler of the AIRNavigationController will be called
+    // synchronously from this context, but AFTER this block is called. To
+    // get around this, we call it async.
+    DispatchQueue.main.async(execute: {
+      viewController.onTransitionCompleted?()
+      viewController.emitEvent("onEnterTransitionComplete", body: nil)
+    })
+  }
     
-    func internalResetToReactViewControllers(
-        _ viewController: InternalReactViewControllerProtocol,
-        animated: Bool,
-        delay: Int64 = DELAY) {
-        
-        // we debounce this call globally
-        if (IN_PROGRESS) {
-            return
-        }
-        IN_PROGRESS = true
-        
-        viewController.eagerNavigationController = self
-        
-        // this should never evaluate true, but is here just to trigger loadView()
-        guard (viewController.viewController().view != nil) else {
-            IN_PROGRESS = false
-            return
-        }
-        
-        
-        let realPush: () -> Void = { [weak self] in
-            IN_PROGRESS = false
-            viewController.onNavigationBarTypeUpdated = nil
-            viewController.isPendingNavigationTransition = false
-            viewController.isCurrentlyTransitioning = true
+  func internalResetToReactViewControllers(
+      _ viewController: InternalReactViewControllerProtocol,
+      animated: Bool,
+      delay: Int64 = DELAY) {
 
-            if let navController = ReactNavigationCoordinator.sharedInstance.topNavigationController(), let transition = self?.getResetTransition() {
-                navController.view.layer.add(transition, forKey: kCATransition)
-                navController.setViewControllers([viewController.viewController()], animated: false)
+      // we debounce this call globally
+      if (IN_PROGRESS) {
+          return
+      }
+      IN_PROGRESS = true
 
-                viewController.eagerNavigationController = nil
-                viewController.realNavigationDidHappen()
-                self?.transitionCoordinator?.animate(alongsideTransition: nil, completion: { context in
-                    viewController.isCurrentlyTransitioning = false
-                    // The completion handler of the AIRNavigationController will be called
-                    // synchronously from this context, but AFTER this block is called. To
-                    // get around this, we call it async.
-                    DispatchQueue.main.async(execute: {
-                      viewController.onTransitionCompleted?()
-                      viewController.emitEvent("onEnterTransitionComplete", body: nil)
-                    })
-            })
-          }
-        }
-        
-        viewController.isPendingNavigationTransition = true
-        viewController.onNavigationBarTypeUpdated = realPush
-        viewController.startedWaitingForRealNavigation()
-        
-        // we delay pushing the view controller just a little bit (50ms) so that the react view can render
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)) {
-            if (viewController.isPendingNavigationTransition) {
-                print("Push Fallback Timer Called!")
-                realPush()
+      viewController.eagerNavigationController = self
+
+      // this should never evaluate true, but is here just to trigger loadView()
+      guard (viewController.viewController().view != nil) else {
+          IN_PROGRESS = false
+          return
+      }
+
+      let realPush: () -> Void = { [weak self] in
+          IN_PROGRESS = false
+          viewController.onNavigationBarTypeUpdated = nil
+          viewController.isPendingNavigationTransition = false
+          viewController.isCurrentlyTransitioning = true
+
+          if let navController = ReactNavigationCoordinator.sharedInstance.topNavigationController(), let transition = self?.getResetTransition() {
+              navController.view.layer.add(transition, forKey: kCATransition)
+              navController.setViewControllers([viewController.viewController()], animated: false)
+
+              viewController.eagerNavigationController = nil
+              viewController.realNavigationDidHappen()
+
+            if let coordinator = self?.transitionCoordinator {
+              coordinator.animate(alongsideTransition: nil, completion: { context in
+                self?.completeViewControllerTransition(viewController)
+              })
             } else {
-                viewController.eagerNavigationController = nil
-                viewController.onNavigationBarTypeUpdated = nil
+              self?.completeViewControllerTransition(viewController)
             }
         }
-    }
+      }
+
+      viewController.isPendingNavigationTransition = true
+      viewController.onNavigationBarTypeUpdated = realPush
+      viewController.startedWaitingForRealNavigation()
+
+      // we delay pushing the view controller just a little bit (50ms) so that the react view can render
+      DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)) {
+          if (viewController.isPendingNavigationTransition) {
+              print("Push Fallback Timer Called!")
+              realPush()
+          } else {
+              viewController.eagerNavigationController = nil
+              viewController.onNavigationBarTypeUpdated = nil
+          }
+      }
+  }
   
   func getResetTransition() -> CATransition {
     let transition = CATransition.init()
